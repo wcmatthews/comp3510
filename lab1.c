3bd92bd1bc5c4d15e5b9c4dc3bf797da5bc4d893
Walter Matthews <collin.matthews@yahoo.com>
2019-02-06 13:45:53 -0600

    First commit

---
+++ lab1.c
@@ -0,0 +1,106 @@
+
+/*****************************************************************************\
+* Laboratory Exercises COMP 3510                                              *
+* Author: Saad Biaz                                                           *
+* Date  : January 18, 2014                                                    *
+\*****************************************************************************/
+
+/*****************************************************************************\
+*                             Global system headers                           *
+\*****************************************************************************/
+
+
+#include "common.h"
+
+/*****************************************************************************\
+*                             Global data types                               *
+\*****************************************************************************/
+
+
+
+/*****************************************************************************\
+*                             Global definitions                              *
+\*****************************************************************************/
+
+
+
+
+
+/*****************************************************************************\
+*                            Global data structures                           *
+\*****************************************************************************/
+
+
+
+
+/*****************************************************************************\
+*                                  Global data                                *
+\*****************************************************************************/
+
+
+
+
+/*****************************************************************************\
+*                               Function prototypes                           *
+\*****************************************************************************/
+
+void Control(void);
+
+
+
+/*****************************************************************************\
+* function: main()                                                            *
+* usage:    Create an artificial environment for embedded systems. The parent *
+*           process is the "control" process while children process will gene-*
+*           generate events on devices                                        *
+*******************************************************************************
+* Inputs: ANSI flat C command line parameters                                 *
+* Output: None                                                                *
+*                                                                             *
+* INITIALIZE PROGRAM ENVIRONMENT                                              *
+* WHILE JOBS STILL RUNNING                                                    *
+*    CREATE PROCESSES                                                         *
+*    RUN LONG TERM SCHEDULER                                                  *
+*    RUN SHORT TERM SCHEDULER                                                 *
+*    DISPATCH NEXT PROCESS                                                    *
+\*****************************************************************************/
+
+int main (int argc, char **argv) {
+
+   if (Initialization(argc,argv)){
+     Control();
+   }
+} /* end of main function */
+
+/***********************************************************************\
+ * Input : none                                                          *
+ * Output: None                                                          *
+ * Function: Monitor Devices and process events                          *
+ \***********************************************************************/
+void Control(void){
+  int i;
+  Event e; //Added
+
+  Status LastStatus=0;
+  while (1) {
+    printf("%10.3f   Flags = %d - \n ", Now(), Flags);
+    sleep(1); // Just to slow down to have time to see Flags
+    if (Flags != LastStatus){
+      LastStatus = Flags;
+      printf("\n >>>>>>>>>  >>> When: %10.3f  Flags = %d\n", Now(),
+	     Flags);
+       //DisplayEvent()
+    }
+  }
+}
+
+
+/***********************************************************************\
+* Input : None                                                          *
+* Output: None                                                          *
+* Function: This must print out the number of Events buffered not yet   *
+*           not yet processed (Server() function not yet called)        *
+\***********************************************************************/
+void BookKeeping(void){
+  printf("\n >>>>>> Done\n");
+}
